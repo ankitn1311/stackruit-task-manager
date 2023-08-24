ff133eafb01d77a177b8bb6fb8dccdcf467cb681
Ankit Negi <ankitn1311@gmail.com>
2023-08-24 21:42:32 +0530

    Loading state

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,26 +2,43 @@ import SingleTask from "@/components/Task";
 import { useCurrentUser } from "@/hooks/state/useCurrentUser";
 import { Task } from "@/types";
 import { firestore } from "@/utils/firebase";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import {
+  Unsubscribe,
+  collection,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 const TaskList = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [currentUser] = useCurrentUser();
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+  let unsubscribe: Unsubscribe;
+
+  const fetchInitialTasks = async () => {
     const q = query(
       collection(firestore, "tasks"),
       where("user", "==", currentUser)
     );
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const tasksFromSnapshot: Task[] = [];
-      querySnapshot.forEach((doc) => {
-        tasksFromSnapshot.push(doc.data() as Task);
+    setLoading(true);
+    await new Promise((resolve) => {
+      unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const tasksFromSnapshot: Task[] = [];
+        querySnapshot.forEach((doc) => {
+          tasksFromSnapshot.push(doc.data() as Task);
+        });
+        setTasks(tasksFromSnapshot);
+        resolve("done");
       });
-      setTasks(tasksFromSnapshot);
     });
+    setLoading(false);
+  };
 
+  useEffect(() => {
+    fetchInitialTasks();
     return () => {
       unsubscribe();
     };
@@ -30,12 +47,16 @@ const TaskList = () => {
   return (
     <div className="flex flex-1 flex-col gap-6">
       <div className="uppercase text-lg">Tasks</div>
-      <div className="flex flex-col gap-2">
-        {tasks.map((task) => (
-          <SingleTask task={task} key={task.id} />
-        ))}
-        {tasks.length === 0 && <div className="text-grey-700">No tasks</div>}
-      </div>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div className="flex flex-col gap-2">
+          {tasks.map((task) => (
+            <SingleTask task={task} key={task.id} />
+          ))}
+          {tasks.length === 0 && <div className="text-grey-700">No tasks</div>}
+        </div>
+      )}
     </div>
   );
 };
